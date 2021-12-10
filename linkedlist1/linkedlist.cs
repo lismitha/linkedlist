f84de0791fc9f3c93633a5b12966800b0d352dbc
lismitha <lismithaspatel@gmail>
2021-12-10 18:57:22 +0530

    adding simple linkedlist

---
+++ linkedlist1/linkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedlist1
+{
+    internal class linkedlist
+    {
+       static void Main()
+       {
+            string[] num = { "56", "30", "70" };
+            Console.WriteLine("Displaying elements of a Linked List: ");
+            LinkedList<string> myList = new LinkedList<string>(num);
+            foreach (var num1 in myList)
+            {
+                Console.WriteLine(num1);
+            }
+       }
+    }
+}
+
